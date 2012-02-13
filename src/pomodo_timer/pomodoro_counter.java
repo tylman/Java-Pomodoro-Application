deb0954fd4ae3381e8d3a4e10bb4a238b67a9912
Andrew Taylor <roo.tay@gmail.com>
2012-02-12 22:58:33 -0500

    Holds functions for the counting aspect of the program

---
+++ src/pomodo_timer/pomodoro_counter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pomodo_timer;
+
+/**
+ *
+ * @author Andrew
+ */
+public class pomodoro_counter {
+    
+    int pomodoro_count=0;
+    
+    public void increase_pomodoro() 
+    {
+        
+        pomodoro_count+=1;
+    }
+    
+    public void reset_pomodoro()
+    {
+        pomodoro_count=0;
+    }
+    
+    public int get_pomodoro()
+    {
+        return pomodoro_count;
+    }
+    
+      
+}
+
